f3d623be22e105fbd795d914782344c0969fdd95
Hoyoul Kang <hoyoul@gmail.com>
2022-02-16 09:57:34 -0700

    ci: add support for ci jest-junit reports

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
+  reporters: [
+    'default',
+    [ 'jest-junit', { outputDirectory: 'test-results', outputName: 'junit.xml' } ],
+  ],
   testEnvironment: 'node',
+  testMatch: [ '**/*.test.ts' ],
 };
