04e698afa079d999bdfa8f412dd4ba0b102a9d68
Hoyoul Kang <hoyoul@gmail.com>
2022-02-20 16:47:47 -0700

    chore: add jest-extended for more match functions

--- jest.config.js
+++ jest.config.js
@@ -2,13 +2,17 @@
 module.exports = {
   collectCoverageFrom: [ './src/**' ],
   coverageDirectory: 'test-results',
+  globals: { 'ts-jest': { tsconfig: 'tsconfig.json' } },
+  moduleDirectories: [ 'node_modules', 'src' ],
+  moduleFileExtensions: [ 'js', 'jsx', 'ts', 'tsx' ],
   modulePathIgnorePatterns: [ './src/defaults.ts', './src/types.ts' ],
   preset: 'ts-jest',
   reporters: [
     'default',
     [ 'jest-junit', { outputDirectory: 'test-results', outputName: 'junit.xml' } ],
   ],
-  setupFiles: [ './testSetup.js' ],
+  setupFilesAfterEnv: [ './test/setup.ts', 'jest-extended/all' ],
   testEnvironment: 'jsdom',
   testMatch: [ '**/*.test.ts' ],
+  transform: { '^.+\\.(ts|tsx)?$': 'ts-jest' },
 };
--- testSetup.js
+++
@@ -1,5 +0,0 @@
-/*
- * This library injects document, window and other DOM APIs into a Node.js environment.
- */
-/* eslint-disable @typescript-eslint/no-var-requires */
-require('jest-canvas-mock');
