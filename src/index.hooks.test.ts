60b7f20671eb2c31faabe7d4c6458ed7c26e657b
Hoyoul Kang <hoyoul@gmail.com>
2022-03-05 11:36:59 -0700

    test: add resize hook test

--- src/index.hooks.test.ts
+++ src/index.hooks.test.ts
@@ -6,12 +6,14 @@ import * as tester from './utils/tester';
 const ELEMENT_ID = 'chart';
 const DIMENSION_COUNT = 4;
 const DATA_COUNT = 50;
+const EVENT_INIT = { bubbles: true, cancelable: true, view: window };
 
 describe('Hermes Hooks', () => {
   let element: HTMLDivElement;
   let dimensions: t.Dimension[];
   let data: t.Data;
-  const dblclick = new MouseEvent('dblclick', { bubbles: true, cancelable: true, view: window });
+  const dblClickEvent = new MouseEvent('dblclick', EVENT_INIT);
+  // const resizeEvent = new Event('resize', EVENT_INIT);
 
   beforeEach(() => {
     element = document.createElement('div');
@@ -27,15 +29,31 @@ describe('Hermes Hooks', () => {
   });
 
   describe('onReset', () => {
-    it('should call `onReset` hook when double clicking chart', () => {
+    it('should call `onReset` when double clicking chart', () => {
       const onReset = jest.fn();
       const config: t.RecursivePartial<t.Config> = { hooks: { onReset } };
       const { destroy } = tryHermes(element, dimensions, config, data);
 
       expect(onReset).not.toHaveBeenCalled();
 
-      element.dispatchEvent(dblclick);
+      element.dispatchEvent(dblClickEvent);
       expect(onReset).toHaveBeenCalled();
+
+      destroy();
+    });
+  });
+
+  describe('onResize', () => {
+    it('should call `onResize` when chart element resizes', () => {
+      const onResize = jest.fn();
+      const config: t.RecursivePartial<t.Config> = { hooks: { onResize }, resizeThrottleDelay: 0 };
+      const { destroy } = tryHermes(element, dimensions, config, data);
+
+      expect(onResize).toHaveBeenCalled();
+
+      // TODO: ResizeObserver is not calling the resize handler via resize-observer-polyfill
+      // document.dispatchEvent(resizeEvent);
+      // expect(onResize).toHaveBeenCalledTimes(2);
 
       destroy();
     });
