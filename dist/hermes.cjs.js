f422237e4267a4dfac122e7b4fca0800150848ac
Hoyoul Kang <hoyoul@gmail.com>
2022-03-10 23:00:00 -0700

    fix: update utility functions to truncate excess zeros and large numbers

--- dist/hermes.cjs.js
+++ dist/hermes.cjs.js
@@ -91,24 +91,28 @@ const randomNumber = (max, min) => {
 
 const readableNumber = (num, precision = 6) => {
     let readable = num.toString();
+    const absoluteNum = Math.abs(num);
     if (isNaN(num)) {
         readable = 'NaN';
     }
     else if (!Number.isFinite(num)) {
         readable = `${num < 0 ? '-' : ''}Infinity`;
     }
-    else if (!Number.isInteger(num)) {
-        readable = num.toFixed(precision);
-        const absoluteNum = Math.abs(num);
+    else if (absoluteNum !== 0) {
         if (absoluteNum < 0.01 || absoluteNum > 999) {
             readable = num.toExponential(precision);
+        }
+        else if (!Number.isInteger(num)) {
+            readable = num.toFixed(precision);
         }
     }
     return readable;
 };
 const readableTick = (num) => {
     let readable = readableNumber(num);
-    readable = readable.replace(/(\.[0-9]+?)0+(e-?\d+)?$/, '$1$2'); // e.g. 0.750000 => 0.75
+    readable = readable.replace(/(e)\+(\d+)/, '$1$2'); // e.g. 1.8e+5 => 1.8e5
+    readable = readable.replace(/0+(e-?\d+)$/, '$1'); // e.g. 1.200000e-5 => 1.2e-5
+    readable = readable.replace(/(\.[0-9]+?)0+$/, '$1'); // e.g. 0.750000 => 0.75
     readable = readable.replace(/\.(e)/, '$1'); // e.g. 2.e5 => 2e5
     return readable;
 };
