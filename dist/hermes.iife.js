9f80c8be9bbcf90c94d8fd4164f6d4043f1154a9
Caleb Kang <caleb@determined.ai>
2022-06-04 13:00:33 -0600

    chore: update library

--- dist/hermes.iife.js
+++ dist/hermes.iife.js
@@ -288,6 +288,30 @@ var Hermes = (function (exports) {
         }
         return Math.random() * (max - min) + min;
     };
+    const removeInfinityNanSeries = (data) => {
+        const keys = Object.keys(data);
+        const indicesToRemove = {};
+        const filteredData = {};
+        let count = 0;
+        // Find all the series indices to remove.
+        for (const key of keys) {
+            if (count === 0)
+                count = data[key].length;
+            for (const [index, value] of data[key].entries()) {
+                if (!isNumber(value) || (!isNaN(value) && isFinite(value)))
+                    continue;
+                indicesToRemove[index] = true;
+            }
+        }
+        // Filter out all the series based on the remove indices.
+        for (const key of keys) {
+            filteredData[key] = data[key].filter((_, index) => !indicesToRemove[index]);
+        }
+        return {
+            count: count - Object.keys(indicesToRemove).length,
+            data: filteredData,
+        };
+    };
 
     const readableNumber = (num, precision = 6) => {
         let readable = num.toString();
@@ -402,6 +426,7 @@ var Hermes = (function (exports) {
             this.maxValue = maxValue;
             this.max = maxValue;
             this.min = minValue;
+            this.range = maxValue - minValue;
             if (calculate)
                 this.calculate();
         }
@@ -1339,8 +1364,9 @@ var Hermes = (function (exports) {
             const dataValidation = Hermes.validateData(data, this.dimensionsOriginal);
             if (!dataValidation.valid)
                 throw new HermesError(dataValidation.message);
-            this.data = data;
-            this.dataCount = dataValidation.count;
+            const filtered = removeInfinityNanSeries(data);
+            this.data = filtered.data;
+            this.dataCount = filtered.count;
             this.setDimensions(this.dimensionsOriginal, false);
             if (redraw)
                 this.redraw();
