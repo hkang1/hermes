6b4a9c82e903c464fe84535db5e4858b940a27f9
Hoyoul Kang <hoyoul@gmail.com>
2023-02-03 14:51:04 -0700

    chore: update and fix some tests

--- src/utils/interaction.test.ts
+++ src/utils/interaction.test.ts
@@ -31,9 +31,9 @@ describe('interaction utilities', () => {
 
   describe('translate internal filters for filter hook calls', () => {
     const tests: { input: t.InternalFilter, output: t.Filter }[] = [
-      { input: { p0: 0.5, p1: 0.2, value0: 5, value1: 2 }, output: [ 2, 5 ] },
-      { input: { p0: 0.1, p1: 0.2, value0: 1, value1: 2 }, output: [ 1, 2 ] },
-      { input: { p0: 0.8, p1: 0.1, value0: 8, value1: 1 }, output: [ 1, 8 ] },
+      { input: { ...DEFAULT.FILTER, p0: 0.5, p1: 0.2, value0: 5, value1: 2 }, output: [ 2, 5 ] },
+      { input: { ...DEFAULT.FILTER, p0: 0.1, p1: 0.2, value0: 1, value1: 2 }, output: [ 1, 2 ] },
+      { input: { ...DEFAULT.FILTER, p0: 0.8, p1: 0.1, value0: 8, value1: 1 }, output: [ 1, 8 ] },
     ];
 
     describe('internalToFilter', () => {
@@ -56,14 +56,14 @@ describe('interaction utilities', () => {
 
   describe('isFilterEmpty', () => {
     it('should determine if a fiter is empty', () => {
-      const filter = { p0: NaN, p1: NaN, value0: NaN, value1: NaN };
+      const filter = { ...DEFAULT.FILTER, p0: NaN, p1: NaN, value0: NaN, value1: NaN };
       expect(utils.isFilterEmpty(filter)).toBe(true);
     });
 
     it('should determine if a fiter is not empty', () => {
-      const filter0 = { p0: 0.2, p1: 0.6, value0: 20, value1: 60 };
-      const filter1 = { p0: NaN, p1: 0.6, value0: 20, value1: 60 };
-      const filter2 = { p0: 0.2, p1: NaN, value0: 20, value1: 60 };
+      const filter0 = { ...DEFAULT.FILTER, p0: 0.2, p1: 0.6, value0: 20, value1: 60 };
+      const filter1 = { ...DEFAULT.FILTER, p0: NaN, p1: 0.6, value0: 20, value1: 60 };
+      const filter2 = { ...DEFAULT.FILTER, p0: 0.2, p1: NaN, value0: 20, value1: 60 };
       expect(utils.isFilterEmpty(filter0)).toBe(false);
       expect(utils.isFilterEmpty(filter1)).toBe(false);
       expect(utils.isFilterEmpty(filter2)).toBe(false);
@@ -72,29 +72,29 @@ describe('interaction utilities', () => {
 
   describe('isFilterInvalid', () => {
     it('should determine if a filter is invalid', () => {
-      const filter0 = { p0: 0.6, p1: 0.4, value0: 60, value1: 40 };
-      const filter1 = { p0: 0.4, p1: 0.4, value0: 40, value1: 40 };
+      const filter0 = { ...DEFAULT.FILTER, p0: 0.6, p1: 0.4, value0: 60, value1: 40 };
+      const filter1 = { ...DEFAULT.FILTER, p0: 0.4, p1: 0.4, value0: 40, value1: 40 };
       expect(utils.isFilterInvalid(filter0)).toBe(true);
       expect(utils.isFilterInvalid(filter1)).toBe(true);
     });
 
     it('should determine if a filter is valid', () => {
-      const filter = { p0: 0.4, p1: 0.6, value0: 40, value1: 60 };
+      const filter = { ...DEFAULT.FILTER, p0: 0.4, p1: 0.6, value0: 40, value1: 60 };
       expect(utils.isFilterInvalid(filter)).toBe(false);
     });
   });
 
   describe('isIntersectingFilters', () => {
     it('should detect if filters are overlapping', () => {
-      const filter0 = { p0: 0.2, p1: 0.6, value0: 20, value1: 60 };
-      const filter1 = { p0: 0.4, p1: 0.8, value0: 40, value1: 80 };
+      const filter0 = { ...DEFAULT.FILTER, p0: 0.2, p1: 0.6, value0: 20, value1: 60 };
+      const filter1 = { ...DEFAULT.FILTER, p0: 0.4, p1: 0.8, value0: 40, value1: 80 };
       expect(utils.isIntersectingFilters(filter0, filter1)).toBe(true);
       expect(utils.isIntersectingFilters(filter1, filter0)).toBe(true);
     });
 
     it('should detect if filters are not overlapping', () => {
-      const filter0 = { p0: 0.2, p1: 0.4, value0: 20, value1: 40 };
-      const filter1 = { p0: 0.6, p1: 0.8, value0: 60, value1: 80 };
+      const filter0 = { ...DEFAULT.FILTER, p0: 0.2, p1: 0.4, value0: 20, value1: 40 };
+      const filter1 = { ...DEFAULT.FILTER, p0: 0.6, p1: 0.8, value0: 60, value1: 80 };
       expect(utils.isIntersectingFilters(filter0, filter1)).toBe(false);
       expect(utils.isIntersectingFilters(filter1, filter0)).toBe(false);
     });
@@ -102,17 +102,88 @@ describe('interaction utilities', () => {
 
   describe('mergeFilters', () => {
     it('should merge overlapping filters', () => {
-      const filter0 = { p0: 0.2, p1: 0.4, value0: 20, value1: 40 };
-      const filter1 = { p0: 0.6, p1: 0.8, value0: 60, value1: 80 };
-      const expected = { p0: 0.2, p1: 0.8, value0: 20, value1: 80 };
+      const filter0 = {
+        ...DEFAULT.FILTER,
+        p0: 0.2,
+        p1: 0.4,
+        percent0: 0.2,
+        percent1: 0.4,
+        value0: 20,
+        value1: 40,
+      };
+      const filter1 = {
+        ...DEFAULT.FILTER,
+        p0: 0.6,
+        p1: 0.8,
+        percent0: 0.6,
+        percent1: 0.8,
+        value0: 60,
+        value1: 80,
+      };
+      const expected = {
+        ...DEFAULT.FILTER,
+        p0: 0.2,
+        p1: 0.8,
+        percent0: 0.2,
+        percent1: 0.8,
+        value0: 20,
+        value1: 80,
+      };
       expect(utils.mergeFilters(filter0, filter1)).toStrictEqual(expected);
       expect(utils.mergeFilters(filter1, filter0)).toStrictEqual(expected);
     });
 
     it('should merge non-overlapping filters', () => {
-      const filter0 = { p0: 0.2, p1: 0.4, value0: 20, value1: 40 };
-      const filter1 = { p0: 0.6, p1: 0.8, value0: 60, value1: 80 };
-      const expected = { p0: 0.2, p1: 0.8, value0: 20, value1: 80 };
+      const filter0 = {
+        ...DEFAULT.FILTER,
+        p0: 0.2,
+        p1: 0.4,
+        percent0: 0.2,
+        percent1: 0.4,
+        value0: 20,
+        value1: 40,
+      };
+      const filter1 = {
+        ...DEFAULT.FILTER,
+        p0: 0.6,
+        p1: 0.8,
+        percent0: 0.6,
+        percent1: 0.8,
+        value0: 60,
+        value1: 80,
+      };
+      const expected = {
+        ...DEFAULT.FILTER,
+        p0: 0.2,
+        p1: 0.8,
+        percent0: 0.2,
+        percent1: 0.8,
+        value0: 20,
+        value1: 80,
+      };
+      expect(utils.mergeFilters(filter0, filter1)).toStrictEqual(expected);
+      expect(utils.mergeFilters(filter1, filter0)).toStrictEqual(expected);
+    });
+
+    it('should merge filters with NaN, Infinity and -Infinity', () => {
+      const filter0 = {
+        ...DEFAULT.FILTER,
+        hasNaN: true,
+        hasNegativeInfinity: false,
+        hasPositiveInfinity: true,
+      };
+      const filter1 = {
+        ...DEFAULT.FILTER,
+        hasNaN: false,
+        hasNegativeInfinity: true,
+        hasPositiveInfinity: false,
+      };
+      const expected = {
+        ...DEFAULT.FILTER,
+        hasNaN: true,
+        hasNegativeInfinity: true,
+        hasPositiveInfinity: true,
+      };
       expect(utils.mergeFilters(filter0, filter1)).toStrictEqual(expected);
       expect(utils.mergeFilters(filter1, filter0)).toStrictEqual(expected);
     });
