50680b8a97e378d7e1254387c77b11be862763d5
Hoyoul Kang <hoyoul@gmail.com>
2023-06-09 15:52:20 -0600

    chore: remove docs since on separate repo now

--- docs/src/components/Icon.tsx
+++
@@ -1,7 +0,0 @@
-interface Props {
-  name: string;
-}
-
-export default function Icon({ name }: Props) {
-  return (<span className={`icon-${name}`} />);
-}
--- docs/src/types.ts
+++
@@ -1,5 +0,0 @@
-export type Point = { x: number, y: number };
-
-export type RecordKey = string | number | symbol;
-
-export type ValueOf<T> = T[keyof T];
